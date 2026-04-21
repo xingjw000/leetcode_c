5cc7fae49a84aa0445c6bab100964af8158ab944
xingjw000 <xingjw000@gmail.com>
2026-04-22 07:29:36 +0800

    105.buildTree/ half

---
+++ 105.buildTree/main.cpp
@@ -0,0 +1,109 @@
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+  
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> inorderMap;
+        inorderMap.reserve(inorder.size());
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            inorderMap.emplace(inorder[i], i);
+        }
+        
+        if (preorder.size() < 1)
+        {
+            return nullptr;
+        }
+        auto root = new TreeNode(preorder[0]);
+        stack<TreeNode *> stack;
+        stack.push(root);
+
+        for (int i = 1; i < preorder.size(); i++)
+        {
+            auto node = new TreeNode(preorder[i]);
+            auto preNode = stack.top();
+            auto preNodeIndex = preNode->val;
+            auto node_index = inorderMap[node->val];
+            if (node_index < preNodeIndex)
+            {
+                preNode->left = node;
+            }else{
+                stack.pop();
+                while(!stack.empty())
+                {
+                    auto node2 = stack.top();
+                    auto node2_index = inorderMap[node2->val];
+                    if (node_index > node2_index)
+                    {
+                        node2->right = node;
+                    }else{
+                        preNode->right = node;
+                    }
+                }
+            }
+            stack.push(node);
+        }
+        
+    }
+};
+
+void list_print(TreeNode *root){
+    if(root == NULL)
+    {
+        return;
+    }
+    cout << "->" << root->val;
+    list_print(root->left);
+    list_print(root->right);
+}
+
+int main()
+{
+    TreeNode t1_node0(15);
+    TreeNode t1_node1(7);
+    TreeNode t1_node2(20, &t1_node0, &t1_node1);
+    TreeNode t1_node3(9);
+    TreeNode t1_node4(3, &t1_node3, &t1_node2);
+
+    cout << "t1:";
+    list_print(&t1_node4);
+    cout << endl;
+
+    Solution sol;
+    cout << sol.maxDepth(&t1_node4) << endl;
+
+    TreeNode t2_node0(2);
+    TreeNode t2_node1(1, nullptr, &t2_node0);
+
+    cout << "t2:";
+    list_print(&t2_node1);
+    cout << endl;
+    cout << sol.maxDepth(&t2_node1) << endl;
+    return 0;
+}
